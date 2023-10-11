9ed4d407455b28a029b9e9751515a3555d56ba44
Deren <danhluu.business@gmail.com>
2023-10-11 23:53:42 +0700

    11:53 11/10/2023

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,6 +6,18 @@ import { useState, useEffect } from "react"
 import { signIn, signOut, useSession, getProviders } from "next-auth"
 
 const Nav = () => {
+	const isUserLoggedIn = true
+
+	const [providers, setProviders] = useState(null)
+
+	useEffect(() => {
+		const setProviders = async () => {
+			const providers = await getProviders()
+
+			setProviders(response)
+		}
+	}, [])
+
 	return (
 		<nav className="flex-between w-full mb-16 pt-3">
 			<Link href="/" className="flex gap-2 flex-center">
@@ -14,6 +26,25 @@ const Nav = () => {
 			</Link>
 
 			{/* Mobile Navigation*/}
+			<div className="sm:flex hidden">
+				{isUserLoggedIn ? (
+					<div className="flex gap-3 md:gap-5">
+						<Link href="/create-prompt" className="black_btn">
+							Create Post
+						</Link>
+
+						<button type="buttn" onClick={signOut} className="outline_btn">
+							Sign Out
+						</button>
+
+						<Link href="/profile">
+							<Image src="/assets/icons/tick.svg" width={37} height={37} className="rounded-full" alt="profile" />
+						</Link>
+					</div>
+				) : (
+					<></>
+				)}
+			</div>
 		</nav>
 	)
 }
